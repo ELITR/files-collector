132d9d8d33ea3b5fd79914ebbda0beb458d0f335
DanielSuchy <daniel.suchy@gmail.com>
2019-04-04 21:41:51 +0200

    template pro program dne

--- files_collector/daily_schedule.py
+++ files_collector/daily_schedule.py
@@ -4,4 +4,4 @@ bp = Blueprint('daily_schedule', __name__, url_prefix='/daily_schedule')
 
 @bp.route('/', methods=('GET', 'POST'))
 def presenters():
-    return 'Hello daily schedule'
+    return render_template('daily_schedule/daily_schedule.html')
--- tests/test_daily_schedule.py
+++ tests/test_daily_schedule.py
@@ -2,4 +2,4 @@ from files_collector import daily_schedule
 
 def test_hello(client):
     response = client.get('/daily_schedule/')
-    assert response.data == b'Hello daily schedule'
+    assert "Program dne" in response.get_data(as_text=True)

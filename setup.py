b3b09e051d1e5f8d6354f0f637182514be5e9e57
DanielSuchy <daniel.suchy@gmail.com>
2019-04-04 14:52:39 +0200

    setup + gitignore

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='files-collector',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+    description = 'A simple web-based tool for collecting supplementary files for presentations and speeches for the project ELITR (elitr.eu).'
+)

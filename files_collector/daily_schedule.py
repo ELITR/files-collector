e590462ebedd9f008255aea13e6cf5347d2e249f
DanielSuchy <daniel.suchy@gmail.com>
2019-04-07 18:22:14 +0200

    chybová hláška v případě nahrání ascii souboru

--- files_collector/daily_schedule.py
+++ files_collector/daily_schedule.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 from werkzeug.utils import secure_filename
 from .folder_structure import FolderStructure
 
@@ -10,7 +10,17 @@ def upload_daily_schedule():
         file = request.files['file']
         file_path = 'D:\\program_dne\\' + secure_filename(file.filename)
         file.save(file_path)
-        fd = FolderStructure(file_path)
-        fd.create_folders()
+        flash(create_folders(file_path))
 
     return render_template('daily_schedule/daily_schedule.html')
+
+def create_folders(daily_schedule):
+    message = 'Načtení programu dne se povedlo'
+
+    try:
+        fd = FolderStructure(daily_schedule)
+        fd.create_folders()
+    except:
+        message = 'Prosím nastavte kódování souboru na utf a zopakujte'
+
+    return message
--- tests/test_daily_schedule.py
+++ tests/test_daily_schedule.py
@@ -16,3 +16,12 @@ def test_upload_file(client):
     os.remove('D:\\program_dne\\program_dne.txt') #Does not exist -> Exception
     os.rmdir('D:\\\\prezentace\\prednaska_1')
     os.rmdir('D:\\\\prezentace\\prednaska_2')
+
+def test_incorrect_file(client):
+    file = open('D:\\files-collector\\tests\\ascii_program_dne_2019-04-01.txt', 'rb')
+    data = {
+        'file': (file, 'program_dne.txt')
+    }
+    res = client.post('/daily_schedule/', data=data)
+    assert res.status_code == 200
+    assert "Prosím nastavte kódování souboru na utf a zopakujte" in res.get_data(as_text=True)

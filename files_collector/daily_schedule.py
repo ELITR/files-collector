c8910c1f2340fff81e06bd4a9e462c769b9f8109
DanielSuchy <daniel.suchy@gmail.com>
2019-04-09 21:03:03 +0200

    zobrazení složek v sidebaru

--- files_collector/daily_schedule.py
+++ files_collector/daily_schedule.py
@@ -1,19 +1,23 @@
 from flask import Blueprint, render_template, request, flash
 from werkzeug.utils import secure_filename
 from .folder_structure import FolderStructure
+from .folder_browser import FolderBrowser
 import mimetypes
 
 bp = Blueprint('daily_schedule', __name__, url_prefix='/daily_schedule')
 
 @bp.route('/', methods=('GET', 'POST'))
 def upload_daily_schedule():
+    menu = []
     if request.method == 'POST':
         file = request.files['file']
         file_path = 'D:\\program_dne\\' + secure_filename(file.filename)
         file.save(file_path)
         flash(create_folders(file_path))
+        fd = FolderBrowser('D:\\prezentace\\')
+        menu = fd.folder_names
 
-    return render_template('daily_schedule/daily_schedule.html', menu = ['test', 'test2', 'test3'])
+    return render_template('daily_schedule/daily_schedule.html', menu = menu)
 
 def create_folders(daily_schedule):
     message = 'Načtení programu dne se povedlo'
--- tests/test_daily_schedule.py
+++ tests/test_daily_schedule.py
@@ -34,3 +34,13 @@ def test_word_file(client):
     res = client.post('/daily_schedule/', data=data)
     assert res.status_code == 200
     assert "Prosím vkládejte pouze textové soubory (ne word, excel apod.)" in res.get_data(as_text=True)
+
+def test_sidebar(client):
+    file = open('D:\\files-collector\\tests\\utf8_program_dne_2019-04-01.txt', 'rb')
+    data = {
+        'file': (file, 'program_dne.txt')
+    }
+    res = client.post('/daily_schedule/', data=data)
+    assert res.status_code == 200
+    assert "prednaska_1" in res.get_data(as_text=True)
+    assert "prednaska_1" in res.get_data(as_text=True)

4fdd785af0661b387cad4efc97b955e4cd9c3d81
DanielSuchy <daniel.suchy@gmail.com>
2019-04-12 20:22:55 +0200

    zobrazí seznam souborů v dané složce

--- files_collector/file_slot.py
+++ files_collector/file_slot.py
@@ -10,6 +10,15 @@ def file_slot(slot_url):
     folder_names = root.folder_names
 
     if slot_url in folder_names:
-        return render_template('file_slot/file_slot.html', menu = folder_names)
+        return render_file_slot(slot_url, folder_names)
+    else:
+        return abort(404)
 
-    return abort(404)
+def render_file_slot(slot_url, folder_names):
+    current_dir = FolderBrowser('D:\\prezentace\\' + slot_url + '\\')
+    current_dir.list_files()
+    file_names = current_dir.file_names
+    return render_template('file_slot/file_slot.html',
+                            menu = folder_names,
+                            file_names = file_names
+                          )
--- tests/test_file_slot.py
+++ tests/test_file_slot.py
@@ -25,3 +25,15 @@ def test_incorrect_url(client):
     os.remove('D:\\program_dne\\program_dne.txt') #Does not exist -> Exception
     os.rmdir('D:\\\\prezentace\\prednaska_1')
     os.rmdir('D:\\\\prezentace\\prednaska_2')
+
+def test_list_files(client):
+    path = 'D:\\prezentace\\prez1\\'
+    os.mkdir(path)
+    open(path + 'file.pptx', 'a').close()
+
+    res = client.get('/prez1')
+    assert res.status_code == 200
+    assert "file.pptx" in res.get_data(as_text=True)
+
+    os.remove('D:\\prezentace\\prez1\\file.pptx')
+    os.rmdir('D:\\\\prezentace\\prez1')

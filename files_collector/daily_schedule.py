d8d33198964cada2d9ed1288da0a53392b7447c1
DanielSuchy <daniel.suchy@gmail.com>
2019-04-12 15:14:21 +0200

    oprava - zobrazování prohlížeče složek i pro metodu get

--- files_collector/daily_schedule.py
+++ files_collector/daily_schedule.py
@@ -8,15 +8,14 @@ bp = Blueprint('daily_schedule', __name__, url_prefix='/daily_schedule')
 
 @bp.route('/', methods=('GET', 'POST'))
 def upload_daily_schedule():
-    menu = []
     if request.method == 'POST':
         file = request.files['file']
         file_path = 'D:\\program_dne\\' + secure_filename(file.filename)
         file.save(file_path)
         flash(create_folders(file_path))
-        fd = FolderBrowser('D:\\prezentace\\')
-        menu = fd.folder_names
 
+    fd = FolderBrowser('D:\\prezentace\\')
+    menu = fd.folder_names
     return render_template('daily_schedule/daily_schedule.html', menu = menu)
 
 def create_folders(daily_schedule):

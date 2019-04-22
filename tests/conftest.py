598ecf1498469c3d2a1e95e49023e5b38ee03c33
DanielSuchy <daniel.suchy@gmail.com>
2019-04-22 16:48:24 +0200

    automatické vyčistění složky před začátkem testování

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-import pytest
+import pytest, os, shutil
 from files_collector import create_app
 
 @pytest.fixture
@@ -7,6 +7,7 @@ def app():
     app = create_app({
         'TESTING': True
     })
+    clean_root_directory()
 
     yield app
 
@@ -18,3 +19,12 @@ def client(app):
 @pytest.fixture
 def runner(app):
     return app.test_cli_runner()
+
+def clean_root_directory():
+    folder = 'D:\\prezentace\\'
+    for the_file in os.listdir(folder):
+        file_path = os.path.join(folder, the_file)
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
